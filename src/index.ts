ed1f55c5bcdab10deb00753cb9c3a5441f20ee48
Marc Campbell <marc.e.campbell@gmail.com>
2026-08-12 08:26:09 -0700

    chore: bump adversary patch version [skip-ci]

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { registerDepotRules } from "./rules/definitions.js";
 export function createApp(): Adversary {
   const app = new Adversary({
     name: "depotci",
-    version: "0.1.0",
+    version: "0.0.16",
     review: { maximumFindings: 8 },
   });
   registerDepotRules(app);
